9d5327d5be131159b5071e781f41867c3b016791
Lana Holmes <lana.lee.holmes@gmail.com>
2018-11-16 02:12:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ playlist['Michael Jackson'] = 'PYT'
 function updatePlaylist(playlist, artistName, songTitle) {
   return playlist[artistName] = songTitle;
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  return playlist
+}
